1c69ac70eb61882244a3bd9c754fdb9caf30eaac
GSKcameraman <gskcameraman@gmail.com>
2023-03-20 02:49:34 -0400

    state to report

---
+++ GOAP/Assets/Scripts/GOAP/GInventory.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GInventory
+{
+    List<GameObject> items = new List<GameObject>();
+}
---
+++ GOAP/Assets/Scripts/Spawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject patientPrefab;
+    public int numPatients;
+    private int i = 0;
+    public float invterval = 0.5f;
+    float currentTime = 0;
+
+    bool invoked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SpawnPatient()
+    {
+        Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+        invoked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( i < numPatients)
+        {
+            currentTime += Time.deltaTime;
+            if ( currentTime >= invterval)
+            {
+                Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+                currentTime = 0;
+            }
+        }
+        else
+        {
+            if (!invoked)
+            {
+                Invoke("SpawnPatient", Random.Range(2, 10));
+                invoked = true;
+            }
+        }
+    }
+}
